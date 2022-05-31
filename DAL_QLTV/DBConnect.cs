061a22578d7e1c9d6438e269dc48fad966ed8840
HuyHoang22 <70369181+HuyHoang22@users.noreply.github.com>
2022-05-31 10:15:30 +0700

    Add files via upload

---
+++ DAL_QLTV/DBConnect.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL_QLTV
+{
+    public class DBConnect
+    {
+        protected SqlConnection _conn = new SqlConnection("Data Source= BOXER\\SQLEXPRESS;Initial Catalog=QLTV;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+    }
+}
